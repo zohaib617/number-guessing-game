255b747bb42bd83e66371c379be760d2e7395b96
zohaib617 <160512319+zohaib617@users.noreply.github.com>
2024-04-07 15:37:20 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+
+
+const randomnumber = Math.floor(Math.random() * 6 +1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNmuber",
+        type: "number",
+        message: "plese guess a number 1 to 6",
+
+    },
+]);
+if(answer.userGuessedNmuber === randomnumber){
+    console.log("Congratulation you guessed right nnumber.")
+}else{
+    console.log("You guessed wrong number.")
+}
